fda7bf5d46c6da9dc9225aad7e58901739f8385a
hanatruongpv <hanatruongpv@gmail.com>
2025-08-16 17:17:59 +0700

    Add ProductCard component

--- src/lib/product.js
+++ src/lib/product.js
@@ -1,1 +1,5 @@
-console.log("product");
+export const products = [
+    { id: "p1", name: "Áo thun Basic", price: 99000, category: "Áo", rating: 4.5, stock: 20, tags: ["new"], img: "/img/p1.jpg" },
+    
+  ];
+  
